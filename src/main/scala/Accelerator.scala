add1b54689f5a3329f00ddcac53e1acb8dbd4032
tacecapSx <martin.handest@gmail.com>
2023-11-15 15:15:44 +0100

    added FSMD skeleton, currently copies input

--- src/main/scala/Accelerator.scala
+++ src/main/scala/Accelerator.scala
@@ -13,6 +13,53 @@ class Accelerator extends Module {
 
   })
 
-  //Write here your code
+  val idle :: read :: write :: done :: Nil = Enum (4)
 
+  io.done := false.B
+  io.address := 0.U
+  io.writeEnable := false.B
+  io.dataWrite := 0.U
+
+  //Write here your code
+  val stateReg = RegInit(done)
+  val iteratorReg = RegInit(0.U(10.W))
+  val addressReg = RegInit(0.U(10.W))
+
+  switch(stateReg) {
+    is(write) {
+      io.address := addressReg
+      io.writeEnable := true.B
+      io.dataWrite := 255.U
+      stateReg := idle
+    }
+    is(idle) {
+      when(iteratorReg === 399.U) {
+        stateReg := done
+      }.otherwise {
+          stateReg := read
+          
+          addressReg := iteratorReg
+
+          iteratorReg := iteratorReg + 1.U
+      }
+    }
+    is(read) {
+      io.address := addressReg
+      when(io.dataRead != 0.U) {
+        stateReg := write
+        addressReg := iteratorReg + 399.U
+      }
+      .otherwise {
+        stateReg := idle
+      }
+    }
+    is(done) {
+      when(io.start) {
+        stateReg := idle
+      }
+      .otherwise {
+        io.done := true.B
+      }
+    }
+  }
 }
